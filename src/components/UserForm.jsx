828d350c9c69e1b6c330a2f72a0cdced3ba14698
Alejandro Roa <alejodev04@gmail.com>
2020-02-22 10:34:02 -0400

    add userForm

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
+
+class UserForm extends Component {
+    render(){
+        // handleSubmit: pasarsele dicha propuedad al onSubmit del form
+        // previente el comportamiento por defecto, pasa la validación y si funciona
+        // ejecuta el onSubmit 
+
+        const { handleSubmit } = this.props
+        return (
+            <form onSubmit={handleSubmit}>
+                <Field name="name" component="input" />
+                <Field name="lastname" component="input"/>
+                <input type="submit" value="Save"/>
+            </form>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'user' // el name form debe ser único para que no choque con otros forms
+})(UserForm)
